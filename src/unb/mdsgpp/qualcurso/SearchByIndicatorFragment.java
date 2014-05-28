d795bc2b538e33d7f30372d07b853535db139312
Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>
2014-05-28 12:30:05 -0300

    new spinner
    Signed-off-by: Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>

--- src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
+++ src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
@@ -9,8 +9,8 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Spinner;
 
-public class SearchByIndicatorFragment extends Fragment{
-	
+public class SearchByIndicatorFragment extends Fragment {
+
 	public SearchByIndicatorFragment() {
 		super();
 	}
@@ -20,37 +20,51 @@ public class SearchByIndicatorFragment extends Fragment{
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.search_fragment, container,
 				false);
-		
+
 		Spinner spinner1 = (Spinner) rootView
 				.findViewById(R.id.course_institution);
-		
+
 		spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
-				
+
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
-				
+
 			}
 		});
-		Spinner spinner2 = (Spinner) rootView
-				.findViewById(R.id.field);
-		
+		Spinner spinner2 = (Spinner) rootView.findViewById(R.id.field);
+
 		spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
-				
+
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
-				
+
+			}
+		});
+		Spinner spinner3 = (Spinner) rootView.findViewById(R.id.year);
+
+		spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+
 			}
 		});
 		return rootView;
