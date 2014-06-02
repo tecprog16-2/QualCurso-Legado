db3d924fc5e4dad5165ee93a10fc55f06b3ca7fc
Fabio Teixeira <fabio1079@gmail.com>
2014-06-01 21:58:58 -0300

    Added event on checkbox MAX to disable secondNumber when this checkbox is checked

--- src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
+++ src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
@@ -18,6 +18,8 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -28,8 +30,7 @@ public class SearchByIndicatorFragment extends Fragment {
 	public SearchByIndicatorFragment() {
 		super();
 	}
-	
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
@@ -39,7 +40,7 @@ public class SearchByIndicatorFragment extends Fragment {
             throw new ClassCastException(activity.toString()+" must implement BeanListCallbacks.");
         }
 	}
-	
+
 	@Override
     public void onDetach() {
         super.onDetach();
@@ -51,9 +52,6 @@ public class SearchByIndicatorFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.search_fragment, container,
 				false);
-		
-		
-		
 		
 		final Spinner listSelectionSpinner = (Spinner) rootView
 				.findViewById(R.id.course_institution);
@@ -71,6 +69,7 @@ public class SearchByIndicatorFragment extends Fragment {
 
 			}
 		});
+
 		final Spinner filterFieldSpinner = (Spinner) rootView.findViewById(R.id.field);
 
 		filterFieldSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -102,24 +101,34 @@ public class SearchByIndicatorFragment extends Fragment {
 
 			}
 		});
-		
+
 		final CheckBox maximum = (CheckBox) rootView.findViewById(R.id.maximum);
 		final EditText firstNumber = (EditText) rootView.findViewById(R.id.firstNumber);
 		final EditText secondNumber = (EditText) rootView.findViewById(R.id.secondNumber);
 		Button searchButton = (Button) rootView.findViewById(R.id.buttonSearch);
-		
+
 		OnClickListener listener = new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
 				int number1, number2, year, max, listSelectionPosition;
 				String filterField;
 
-				number1 = Integer.parseInt(firstNumber.getText().toString());
-				number2 = Integer.parseInt(secondNumber.getText().toString());
+				if( firstNumber.getText().length() == 0) {
+					firstNumber.setText("0");
+				}
+
+				if( secondNumber.getText().length() == 0 ) {
+					maximum.setChecked(true);
+				}
+
+				String firstNumberValue = firstNumber.getText().toString();
+				String secondNumberValue = secondNumber.getText().toString();
+
+				number1 = Integer.parseInt(firstNumberValue);
+				number2 = maximum.isChecked() ? -1 : Integer.parseInt(secondNumberValue);
 				listSelectionPosition = listSelectionSpinner.getSelectedItemPosition();
-				
-				
+
 				if( yearSpinner.getSelectedItemPosition() != 0 ) {
 					year = Integer.parseInt(yearSpinner.getSelectedItem().toString());
 				} else {
@@ -131,7 +140,7 @@ public class SearchByIndicatorFragment extends Fragment {
 				}else{
 					max = number2;
 				}
-				
+
 				switch (filterFieldSpinner.getSelectedItemPosition()) {
 				case 0:
 					filterField = "";
@@ -140,51 +149,51 @@ public class SearchByIndicatorFragment extends Fragment {
 				case 1:
 					filterField = new Evaluation().fieldsList().get(5);
 					break;
-					
+
 				case 2:
 					filterField = new Evaluation().fieldsList().get(6);
 					break;
-					
+
 				case 3:
 					filterField = new Evaluation().fieldsList().get(8);
 					break;
-					
+
 				case 4:
 					filterField = new Evaluation().fieldsList().get(9);
 					break;
-					
+
 				case 5:
 					filterField = new Evaluation().fieldsList().get(10);
 					break;
-					
+
 				case 6:
 					filterField = new Evaluation().fieldsList().get(13);
 					break;
-					
+
 				case 7:
 					filterField = new Book().fieldsList().get(2);
 					break;
-					
+
 				case 8:
 					filterField = new Book().fieldsList().get(1);
 					break;
-					
+
 				case 9:
 					filterField = new Book().fieldsList().get(3);
 					break;
-					
+
 				case 10:
 					filterField = new Book().fieldsList().get(4);
 					break;
-					
+
 				case 11:
 					filterField = new Article().fieldsList().get(1);
 					break;
-					
+
 				case 12:
 					filterField = new Article().fieldsList().get(2);
 					break;
-					
+
 				default:
 					filterField = "";
 					break;
@@ -197,38 +206,51 @@ public class SearchByIndicatorFragment extends Fragment {
 				if(filterField == "") {
 					//TODO exception 
 				} else {
-					
+
 						switch (listSelectionPosition) {
 						case 0:
 							callInstitutionList(min, max, year, filterField);
 							break;
-				
+
 						case 1:
 							callCourseList(min, max, year, filterField);
 							break;
-				
+
 						case 2:
 							callInstitutionList(min, max, year, filterField);
 							break;
-					
+
 						default:
 							break;
-					
-							}
+						}
 				}
 			}
-			
+
 			private void callInstitutionList(int min, int max, int year, String filterField){
 				ArrayList<Institution> beanList = Institution.getInstitutionsByEvaluationFilter(filterField, year, min, max);
 				beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,filterField, year, min, max), R.id.search_list);
 			}
+
 			private void callCourseList(int min, int max, int year, String filterField){
 				ArrayList<Course> beanList = Course.getCoursesByEvaluationFilter(filterField, year, min, max);
 				beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,filterField, year, min, max), R.id.search_list);
 			}
 		};
-			searchButton.setOnClickListener(listener);
-		
+
+		searchButton.setOnClickListener(listener);
+
+		// Event to disable second number when MAX is checked
+		maximum.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if( maximum.isChecked() ) {
+					secondNumber.setEnabled(false);
+				} else {
+					secondNumber.setEnabled(true);
+				}
+			}
+		});
+
 		return rootView;
 	}
 
