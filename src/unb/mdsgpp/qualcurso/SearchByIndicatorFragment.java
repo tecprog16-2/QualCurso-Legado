be0441800920b0bd70b2fb992c30b0c70e234454
André Bernardes <andrebsguedes@gmail.com>
2014-06-23 22:48:01 -0300

    Refactoring SearchByIndicatorFragment due to low code quality.
    (code_refactoring)
    
    Signed-off-by: Andre Bernardes <andrebsguedes@gmail.com>
    Signed-off-by: Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>

--- src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
+++ src/unb/mdsgpp/qualcurso/SearchByIndicatorFragment.java
@@ -56,6 +56,14 @@ public class SearchByIndicatorFragment extends Fragment {
         super.onDetach();
         beanCallbacks = null;
     }
+	
+	Spinner listSelectionSpinner = null;
+	Spinner filterFieldSpinner = null;
+	Spinner yearSpinner = null;
+	CheckBox maximum = null;
+	EditText firstNumber = null;
+	EditText secondNumber = null;
+	Button searchButton = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,18 +71,35 @@ public class SearchByIndicatorFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.search_fragment, container,
 				false);
 		
-		final Spinner listSelectionSpinner = (Spinner) rootView
+		listSelectionSpinner = (Spinner) rootView
 				.findViewById(R.id.course_institution);
-		
-		final Spinner filterFieldSpinner = (Spinner) rootView.findViewById(R.id.field);
+		filterFieldSpinner = (Spinner) rootView.findViewById(R.id.field);
 		filterFieldSpinner.setAdapter(new ArrayAdapter<Indicator>(getActivity().getApplicationContext(), R.layout.simple_textview,Indicator.getIndicators()));
-		final Spinner yearSpinner = (Spinner) rootView.findViewById(R.id.year);
-		final CheckBox maximum = (CheckBox) rootView.findViewById(R.id.maximum);
-		final EditText firstNumber = (EditText) rootView.findViewById(R.id.firstNumber);
-		final EditText secondNumber = (EditText) rootView.findViewById(R.id.secondNumber);
-		Button searchButton = (Button) rootView.findViewById(R.id.buttonSearch);
+		yearSpinner = (Spinner) rootView.findViewById(R.id.year);
+		maximum = (CheckBox) rootView.findViewById(R.id.maximum);
+		firstNumber = (EditText) rootView.findViewById(R.id.firstNumber);
+		secondNumber = (EditText) rootView.findViewById(R.id.secondNumber);
+		searchButton = (Button) rootView.findViewById(R.id.buttonSearch);
+		
+		searchButton.setOnClickListener(getClickListener());
+		
+		// Event to disable second number when MAX is checked
+		maximum.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if( maximum.isChecked() ) {
+					secondNumber.setEnabled(false);
+				} else {
+					secondNumber.setEnabled(true);
+				}
+			}
+		});
 
-		OnClickListener listener = new OnClickListener() {
+		return rootView;
+	}
+	
+	public OnClickListener getClickListener(){
+		return new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
@@ -106,84 +131,69 @@ public class SearchByIndicatorFragment extends Fragment {
 				}else{
 					max = number2;
 				}
-					this.updateSearchList(number1, max, year, listSelectionPosition, ((Indicator)filterFieldSpinner.getItemAtPosition(filterFieldSpinner.getSelectedItemPosition())));
+					updateSearchList(number1, max, year, listSelectionPosition, ((Indicator)filterFieldSpinner.getItemAtPosition(filterFieldSpinner.getSelectedItemPosition())));
 			
 			}
 
-			private void updateSearchList(int min, int max, int year, int listSelectionPosition, Indicator filterField) {
-				if(filterField.getValue() == Indicator.DEFAULT_INDICATOR) {
-					Context c = QualCurso.getInstance();
-					String emptySearchFilter = getResources().getString(R.string.empty_search_filter);
-
-					Toast toast = Toast.makeText(c, emptySearchFilter, Toast.LENGTH_SHORT);
-					toast.show();
-				} else {
-						switch (listSelectionPosition) {
-						case 0:
-							listSelectionSpinner.setSelection(listSelectionSpinner.getAdapter().getCount()-1);
-							yearSpinner.setSelection(yearSpinner.getAdapter().getCount()-1);
-
-							callInstitutionList(min, max, year, filterField);
-							break;
-
-						case 1:
-							callCourseList(min, max, year, filterField);
-							break;
-
-						case 2:
-							callInstitutionList(min, max, year, filterField);
-							break;
-
-						default:
-							break;
-						}
-				}
-			}
-
-			private void callInstitutionList(int min, int max, int year, Indicator filterField){
-				Calendar c = Calendar.getInstance();
-				Search search = new Search();
-				search.setDate(new Date(c.getTime().getTime()));
-				search.setYear(year);
-				search.setOption(1);
-				search.setIndicator(filterField);
-				search.setMinValue(min);
-				search.setMaxValue(max);
-				search.save();
-				ArrayList<Institution> beanList = Institution.getInstitutionsByEvaluationFilter(search);
-				beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,search), R.id.search_list);
-			}
-
-			private void callCourseList(int min, int max, int year, Indicator filterField){
-				Calendar c = Calendar.getInstance();
-				Search search = new Search();
-				search.setDate(new Date(c.getTime().getTime()));
-				search.setYear(year);
-				search.setOption(0);
-				search.setIndicator(filterField);
-				search.setMinValue(min);
-				search.setMaxValue(max);
-				search.save();
-				ArrayList<Course> beanList = Course.getCoursesByEvaluationFilter(search);
-				beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,search), R.id.search_list);
-			}
 		};
+	}
+	
+	private void callInstitutionList(int min, int max, int year, Indicator filterField){
+		Calendar c = Calendar.getInstance();
+		Search search = new Search();
+		search.setDate(new Date(c.getTime().getTime()));
+		search.setYear(year);
+		search.setOption(1);
+		search.setIndicator(filterField);
+		search.setMinValue(min);
+		search.setMaxValue(max);
+		search.save();
+		ArrayList<Institution> beanList = Institution.getInstitutionsByEvaluationFilter(search);
+		beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,search), R.id.search_list);
+	}
 
-		searchButton.setOnClickListener(listener);
+	private void callCourseList(int min, int max, int year, Indicator filterField){
+		Calendar c = Calendar.getInstance();
+		Search search = new Search();
+		search.setDate(new Date(c.getTime().getTime()));
+		search.setYear(year);
+		search.setOption(0);
+		search.setIndicator(filterField);
+		search.setMinValue(min);
+		search.setMaxValue(max);
+		search.save();
+		ArrayList<Course> beanList = Course.getCoursesByEvaluationFilter(search);
+		beanCallbacks.onBeanListItemSelected(SearchListFragment.newInstance(beanList,search), R.id.search_list);
+	}
+	
+	private void updateSearchList(int min, int max, int year, int listSelectionPosition, Indicator filterField) {
+		if(filterField.getValue() == Indicator.DEFAULT_INDICATOR) {
+			Context c = QualCurso.getInstance();
+			String emptySearchFilter = getResources().getString(R.string.empty_search_filter);
 
-		// Event to disable second number when MAX is checked
-		maximum.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if( maximum.isChecked() ) {
-					secondNumber.setEnabled(false);
-				} else {
-					secondNumber.setEnabled(true);
+			Toast toast = Toast.makeText(c, emptySearchFilter, Toast.LENGTH_SHORT);
+			toast.show();
+		} else {
+				switch (listSelectionPosition) {
+				case 0:
+					listSelectionSpinner.setSelection(listSelectionSpinner.getAdapter().getCount()-1);
+					yearSpinner.setSelection(yearSpinner.getAdapter().getCount()-1);
+
+					callInstitutionList(min, max, year, filterField);
+					break;
+
+				case 1:
+					callCourseList(min, max, year, filterField);
+					break;
+
+				case 2:
+					callInstitutionList(min, max, year, filterField);
+					break;
+
+				default:
+					break;
 				}
-			}
-		});
-
-		return rootView;
+		}
 	}
 
 }
